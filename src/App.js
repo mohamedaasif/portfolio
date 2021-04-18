5a03494b92e35fec0bbea0af17705cbc4cafbb34
Aasif <aasif2496@gmail.com>
2021-04-18 18:04:16 +0530

    testing again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "./App.scss";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
@@ -11,20 +11,22 @@ import Project from "./components/Project/Project";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
-  // const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-  // useEffect(() => {
-  //   if (window.innerWidth < 780) {
-  //     setIsMobile(true);
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (window.innerWidth < 780) {
+      setIsMobile(true);
+    } else {
+      <Redirect to="/" />
+    }
+  }, []);
 
   return (
     <Router>
       <div className="App">
         <Navbar />
       
-       {/*  {isMobile ? (
+       {isMobile ? (
           <div>
             <Home />
             <About />
@@ -41,14 +43,14 @@ function App() {
               <Route path="/contact" component={Contact} />
             </Switch>
           </div>
-        )} */}
-          <ScrollToTop />
+        )}
+          {/* <ScrollToTop />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/project" component={Project} />
             <Route path="/contact" component={Contact} />
-          </Switch>  
+          </Switch>   */}
         <Footer />
       </div>
     </Router>
