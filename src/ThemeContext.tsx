7e8bf4d55f94bb5e5654bd42c3aedad26323e31b
mohamedaasif <mohamedaasif.md@gmail.com>
2024-09-29 18:35:40 +0530

    add theme to persist on every session

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode, FC } from "react";
+import { createContext, useState, ReactNode, FC, useEffect } from "react";
 
 export interface ThemeContextType {
   darkTheme: boolean;
@@ -10,8 +10,21 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [darkTheme, setDarkTheme] = useState<boolean>(true);
 
+  useEffect(() => {
+    let getTheme: any = sessionStorage.getItem("theme");
+    if (getTheme) {
+      getTheme = JSON.parse(getTheme);
+      setDarkTheme(getTheme);
+    } else {
+      sessionStorage.setItem("theme", JSON.stringify(darkTheme));
+    }
+  }, []);
+
   const toggleTheme = () => {
-    setDarkTheme((prevTheme) => !prevTheme);
+    setDarkTheme((prevTheme) => {
+      sessionStorage.setItem("theme", JSON.stringify(!prevTheme));
+      return !prevTheme;
+    });
   };
 
   return (
