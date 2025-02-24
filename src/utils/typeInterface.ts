5203c33d8947815139b709d4bb0872e7675ce1ce
mohamedaasif <mohamedaasif.md@gmail.com>
2025-02-24 18:46:37 +0530

    Updates

--- src/utils/typeInterface.ts
+++
@@ -1,5 +0,0 @@
-export interface ProjectsProps {
-  setIsArchiveProjects: (value: boolean) => void;
-  projectRef?: React.RefObject<HTMLDivElement | null>;
-  scrollToSection?: (ref: React.RefObject<HTMLDivElement>) => void;
-}
