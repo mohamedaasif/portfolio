35618ecbcbdb4e80d0facc69943306b070f61116
mohamedaasif <mohamedaasif.md@gmail.com>
2025-07-26 10:59:27 +0530

    updated resume

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,11 +14,11 @@ const Home = (props: HomeProps) => {
   );
   const handleViewPdf = () => {
     const pdfUrl =
-      "https://mohamedaasif.github.io/portfolio/mohamed-aasif-frontend-developer.pdf";
+      "https://mohamedaasif.github.io/portfolio/Mohamed_Aasif_FrontendDeveloper.pdf";
 
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "mohamed-aasif-frontend-developer.pdf";
+    link.download = "Mohamed_Aasif_FrontendDeveloper.pdf";
     link.click();
   };
   return (
